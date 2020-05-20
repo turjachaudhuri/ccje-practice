4ff210a6ecb4201501f7f0400ade8bbb690b623d
turjachaudhuri <turjachaudhuri@gmail.com>
2020-05-20 15:24:07 +0530

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -1,4 +1,4 @@
 echo "this script is from github"
 NUM=$(($NUMBERONE + $NUMBERTWO))
 echo "NUM IS $NUM"
-echo "this is complete aain - a true JSR ,  wolf123"
+echo "this is complete aain - a true JSR ,  wolf1234"
